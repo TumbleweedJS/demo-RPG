5626843e6b2d337c66065956ab63443aaebbb127
Arnaud Bonnet de Larbogne <bonnet_b@epitech.eu>
2013-09-16 01:19:11 +0200

    Adding fade in fade out effect

--- script/MapState.js
+++ script/MapState.js
@@ -13,18 +13,28 @@ define(['TW/Utils/inherit', 'TW/GameLogic/GameState'], function(inherit, GameSta
 		GameState.call(this, {
 			name:   "map"
 		});
-
-		/**
+		this.startToDraw = false;
+		this.opacity = 0.0;
+		this.status = "fadeIN";
+		/**mainCanvas
 		 * @property {Map} map
 		 */
 		this.map = null;
 
 		//hack for enable XXState.prototype.onXXX()
-		//delete this.onCreation;
+		delete this.onCreation;
 		//delete this.onDelete;
 	}
 
 	inherit(MapState, GameState);
+
+	MapState.prototype.onCreation = function() {
+		if (!this.fadeInID) {
+		var context = document.getElementById("mainCanvas").getContext("2d");
+		context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+		this.fadeInID = window.setInterval(this.fadeIn.bind(this, document.getElementById("mainCanvas").getContext("2d")), (1/60)*1000);
+		}
+	};
 
 	/**
 	 * set and initialize a new map.
@@ -36,6 +46,30 @@ define(['TW/Utils/inherit', 'TW/GameLogic/GameState'], function(inherit, GameSta
 		this.map = map;
 	};
 
+	MapState.prototype.fadeIn = function(context) {
+		if (this.opacity >= 1.0) {
+			this.status = "fadeOUT";
+			window.clearInterval(this.fadeInID);
+			this.fadeOutID = window.setInterval(this.fadeOut.bind(this, context, {x:80, y:80}), (1/60)*1000);
+			return;
+		}
+		
+		this.opacity+=1/60;
+	};
+
+	MapState.prototype.fadeOut = function(context, posPlayer) {
+		if (this.opacity <= 0.0) {
+			window.clearInterval(this.fadeOutID);
+			this.startToDraw = true;
+			this.status = "";
+			this.opacity = 0.0;
+			return;
+		}
+		this.opacity-=1/60;
+		if (this.opacity < 0)
+			this.opacity = 0;
+	};
+
 	/**
 	 * Draw the start screen.
 	 *
@@ -43,8 +77,22 @@ define(['TW/Utils/inherit', 'TW/GameLogic/GameState'], function(inherit, GameSta
 	 * @param context
 	 */
 	MapState.prototype.draw = function(context) {
+		//if (this.startToDraw === true) {
+		context.save();
 		context.fillStyle = '#FF33FF';
 		context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+		//}
+		if (this.status === "fadeIN") {
+			context.fillStyle = "rgb(0, 0, 0)";
+			context.globalAlpha = this.opacity;
+			context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+		} else if (this.status === "fadeOUT") {
+			console.log("global alpha "+this.opacity);
+			context.fillStyle = "rgb(0, 0, 0)";
+			context.globalAlpha = this.opacity;
+			context.fillRect(0, 0, context.canvas.width, context.canvas.height);	
+		}
+		context.restore();
 	};
 
 	return MapState;
